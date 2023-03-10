bb1a110e43f9e5346e80b3691b0729535fefdb2c
pedrosa <mpedrosa2110@gmail.com>
2023-03-10 15:09:13 -0300

    terminado

--- src/Persona.java
+++ src/Persona.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 public class Persona {
     private String nombre;
     private String apellido;
-    private LocalDateTime fecha_de_nacimiento;
+    private LocalDateTime fechaDeNacimiento;
 
-    public Persona(String nombre, String apellido, LocalDateTime fecha_de_nacimiento ){
+    public Persona(String nombre, String apellido, LocalDateTime fechaDeNacimiento ){
         this.nombre = nombre;
         this.apellido = apellido;
-        this.fecha_de_nacimiento = fecha_de_nacimiento;
+        this.fechaDeNacimiento = fechaDeNacimiento;
     }
 }
